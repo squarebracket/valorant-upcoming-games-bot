fdcc52bc1fa96f7f0d0a4ba0e24a6715c8cd9c37
Chuck Wilson <chuck.wilson+github@gmail.com>
2024-09-25 15:24:35 -0400

    add liquipedia module

---
+++ src/modules/liquipedia.ts
@@ -0,0 +1,116 @@
+import { StreamMapperFunction, TricodeMapper, doRequest, CacheOptions } from "../lib/utils.ts";
+import { League } from "../lib/leagues.ts";
+import { Match } from "../lib/matches.ts";
+import * as config from '../config.ts';
+
+type LiquipediaMatchTeam = {
+  id: number,
+  type: 'team',
+  name: string,
+  icon: string,
+  score: number,
+  status: string,
+  teamtemplate: {
+    name: string,
+    shortname: string,
+    imageurl: string,
+  }
+}
+
+type LiquipediaMatch = {
+  pageid: number,
+  pagename: string,
+  match2id: string,
+  match2bracketid: string,
+  winner: string, // number in a string
+  finished: number, // number as bool
+  section: string,
+  date: string, // datetime in string as format YYYY-MM-DD HH:mm:SS
+  dateexact: number,
+  stream: {[key: string]: string},
+  links: string[], // ?
+  tournament: string,
+  extradata: {
+    timestamp: number,
+    timezoneoffset: string,
+    timezoneid: string,
+  },
+  match2opponents: LiquipediaMatchTeam[],
+  bestof: number,
+}
+
+
+export async function getLiquipedia(
+  tournament: string,
+  league: League,
+  tricodeMapper?: TricodeMapper,
+  streamMapperFn?: StreamMapperFunction,
+): Promise<Match[]> {
+  const apiKey = config['liquipediaApiKey'];
+  const headers = {
+    authorization: `Apikey ${apiKey}`
+  };
+  const url = new URL(`https://api.liquipedia.net/api/v3/match?wiki=valorant&limit=20&conditions=[[tournament::${tournament}]]&streamurls=true&rawstreams=true&order=date%20ASC`);
+  const cacheOptions: CacheOptions = {
+    key: tournament,
+    time: 180,
+  }
+  const matches: LiquipediaMatch[] = (await doRequest(url, { headers, cache: cacheOptions })).result;
+  const newMatches: Match[] = [];
+
+  matches.forEach((match) => {
+    if (match.finished) {
+      return;
+    }
+    const startTime = new Date(match.extradata.timestamp * 1000);
+    const newMatch: Match = {
+      league: league,
+      state: startTime > new Date() ? 'upcoming' : 'live',
+      startTime: startTime,
+    };
+
+    if (match.match2opponents[0] && match.match2opponents[0].name !== '') {
+      newMatch.teamA = {
+        name: match.match2opponents[0].name,
+        code: match.match2opponents[0].teamtemplate.shortname,
+        result: {
+          mapWins: match.match2opponents[0].score,
+        }
+      }
+    }
+
+    if (match.match2opponents[1] && match.match2opponents[1].name !== '') {
+      newMatch.teamB = {
+        name: match.match2opponents[1].name,
+        code: match.match2opponents[1].teamtemplate.shortname,
+        result: {
+          mapWins: match.match2opponents[1].score,
+        }
+      }
+    }
+
+    if (match.bestof) {
+      newMatch.strategy = {
+        type: "bestOf",
+        count: match.bestof,
+      }
+    }
+
+    if (match.stream) {
+      if (match.stream["twitch_en_1"]) {
+        newMatch.stream = new URL(match.stream["twitch_en_1"]);
+      } else if (match.stream["twitch"]) {
+        newMatch.stream = new URL(match.stream["twitch"]);
+      } else if (match.stream['bilibili']) {
+        newMatch.stream = new URL(match.stream['bilibili']);
+      } else if (match.stream['douyu']) {
+        newMatch.stream = new URL(match.stream['douyu']);
+      } else if (match.stream['huya']) {
+        newMatch.stream = new URL(match.stream['huya']);
+      }
+    }
+
+    newMatches.push(newMatch);
+  });
+  return newMatches;
+}
