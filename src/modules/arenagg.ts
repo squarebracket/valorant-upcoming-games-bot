b03a6a0a64eef5f4e18c2e01266d5c3fe2328d66
Chuck Wilson <chuck.wilson+github@gmail.com>
2025-04-19 22:38:33 -0400

    add standings

--- src/modules/arenagg.ts
+++ src/modules/arenagg.ts
@@ -37,7 +37,23 @@ export async function getArenaGG(
 ): Promise<Match[]> {
   const retMatches: Match[] = [];
   const matches: ArenaGGMatch[] = (await doRequest(new URL(`https://api.arenagg.com/v1/matches?competition=${tournament}&limit=256&orderBy=position&sort=ASC`)))
-    .data.filter((match: ArenaGGMatch) => match.status !== 'finished' && match.status !== 'canceled' && match.status !== 'resolved');
+    .data.filter((match: ArenaGGMatch) => match.status !== 'canceled' && match.status !== 'resolved');
+
+  const standings: { [key: string]: { w: number, l: number } } = {};
+  matches.forEach((match) => {
+    if (match.status === 'finished' && match.profiles.length === 2) {
+      match.profiles.forEach((profile) => {
+        if (!standings[profile.participant.name]) {
+          standings[profile.participant.name] = { w: 0, l: 0 };
+        }
+        if (profile.isWinner) {
+          standings[profile.participant.name].w++;
+        } else {
+          standings[profile.participant.name].l++;
+        }
+      })
+    }
+  })
 
   matches.forEach((match) => {
     if (new Date() > new Date(match.startDate * 1000 + match.resultPublishTime*60*1000)) {
@@ -62,6 +78,10 @@ export async function getArenaGG(
         result: {
           mapWins: match.result[teamA.id.toString()] ?? 0,
         },
+        record: {
+          wins: standings[teamA.participant.name].w,
+          losses: standings[teamA.participant.name].l,
+        },
       };
     }
     
@@ -72,6 +92,10 @@ export async function getArenaGG(
         code: teamB.participant.tag ?? tricodeMapper[teamB.participant.name] ?? teamB.participant.name,
         result: {
           mapWins: match.result[teamB.id.toString()] ?? 0,
+        },
+        record: {
+          wins: standings[teamB.participant.name].w,
+          losses: standings[teamB.participant.name].l,
         },
       };
     }
