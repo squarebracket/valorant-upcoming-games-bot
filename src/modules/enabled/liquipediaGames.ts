5a1273f9788ce14d92a17a6d22f2a59c9eacee0c
Chuck Wilson <chuck.wilson+github@gmail.com>
2025-04-19 22:55:46 -0400

    add liquipedia games

---
+++ src/modules/enabled/liquipediaGames.ts
@@ -0,0 +1,58 @@
+import { getLeagueByName, League } from "../../lib/leagues.ts";
+import { Match, Team } from "../../lib/matches.ts";
+import { getLiquipedia } from "../liquipedia.ts";
+
+export async function getMatches(): Promise<Match[]> {
+  try {
+    // const vctCN = await getLeagueByName('VCT CN');
+    // const vctPac = await getLeagueByName('VCT Pacific');
+    // const vctEMEA = await getLeagueByName('VCT EMEA');
+    // const vctAmer = await getLeagueByName('VCT Americas');
+    // const vctMasters = await getLeagueByName('VALORANT Masters');
+    const gcEMEA = await getLeagueByName('Game Changers EMEA');
+    const gcBR = await getLeagueByName('Game Changers BR');
+    // const vcb = await getLeagueByName('Challengers BR');
+    const gcNA = await getLeagueByName('Game Changers NA');
+    const gcLatam = await getLeagueByName('Game Changers LATAM');
+    const gcKr = await getLeagueByName('Game Changers KR');
+    const gcJp = await getLeagueByName('Game Changers JPN');
+    if (gcJp === undefined) {
+      console.error(`can't find league`);
+      throw new Error("can't find league");
+    }
+    if (gcKr === undefined) {
+      console.error(`can't find league`);
+      throw new Error("can't find league");
+    }
+    if (gcLatam === undefined) {
+      console.error(`can't find league`);
+      throw new Error("can't find league");
+    }
+    if (gcNA === undefined) {
+      console.error(`can't find league`);
+      throw new Error("can't find league");
+    }
+    if (gcBR === undefined) {
+      console.error(`can't find league`);
+      throw new Error("can't find league");
+    }
+    if (gcEMEA === undefined) {
+      console.error(`can't find league`);
+      throw new Error("can't find league");
+    }
+    const tourneys = [
+      'VCT 2025: Game Changers Korea Split 1',
+      'VCT 2025: Game Changers EMEA Contenders Series 1',
+    ];
+    const leagueMapper = (tournament: string): League => {
+      if (tournament.includes('Korea')) {
+        return gcKr;
+      } else {
+        return gcEMEA;
+      }
+    }
+    return getLiquipedia(tourneys, leagueMapper);
+  } catch {
+    return [];
+  }
+}
